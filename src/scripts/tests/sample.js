aec9c90f6c454aefe6d641c03b51fc35c97ea9b3
TheHiddenLayer <ubhattac@ucsd.edu>
2021-02-09 16:47:35 -0800

    Configure ESlint to work with pretttier plugin

--- src/scripts/tests/sample.js
+++ src/scripts/tests/sample.js
@@ -1,10 +1,10 @@
 const functions = {
-    addNums: (n1, n2) => {
-       return n1 + n2;
-    },
-    subtractNums: (n1, n2) => {
-       return n1 - n2;
-    }
-}
+  addNums: (n1, n2) => {
+    return n1 + n2;
+  },
+  subtractNums: (n1, n2) => {
+    return n1 - n2;
+  }
+};
 
 module.exports = functions;
--- src/scripts/tests/sample.test.js
+++ src/scripts/tests/sample.test.js
@@ -1,3 +1,7 @@
-const { test } = require("@jest/globals");
-const functions = require("./sample");
-test("Adds 2 and 2 to equal 4", () => { expect(functions.addNums(2, 2)).toBe(4);});
+import { test } from '@jest/globals';
+
+import { addNums } from './sample';
+
+test('Adds 2 and 2 to equal 4', () => {
+  return expect(addNums(2, 2)).toBe(4);
+});
