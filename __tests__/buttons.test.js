4fe32a412c331e4963315967d4826b43afb90381
ryanbuui <57205440+ryanbuui@users.noreply.github.com>
2021-03-09 21:46:26 -0800

    Ran formatter

--- __tests__/buttons.test.js
+++ __tests__/buttons.test.js
@@ -2,9 +2,9 @@ const { redirectToInstructionsPage } = require('../src/scripts/buttons.js');
 
 beforeEach(() => {
   global.window = Object.create(window);
-  window.confirm = jest.fn(() => true)
-  window.prompt = jest.fn(() => 0)
-  window.alert = jest.fn(() => true)
+  window.confirm = jest.fn(() => true);
+  window.prompt = jest.fn(() => 0);
+  window.alert = jest.fn(() => true);
   Object.defineProperty(window, 'location', {
     value: {
       href: '/'
