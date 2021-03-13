0425c7c362e8b16e0875a74efe07eef11dcbc423
yuw006 <yuw006@ucsd.edu>
2021-03-13 15:32:03 -0800

    fix: edit code

--- __tests__/buttons.test.js
+++ __tests__/buttons.test.js
@@ -37,39 +37,38 @@ describe('Task Page Tests', () => {
     expect(buttons.calculateTotalTime(tasks)).toBe(false);
 
     // empty time
-    let task1 = {id: '1', taskName: '', min: '', pomos: 0, done: false};
+    let task1 = { id: '1', taskName: '', min: '', pomos: 0, done: false };
     tasks.push(task1);
     expect(buttons.calculateTotalTime(tasks)).toBe(false);
 
     // not number time
-    task1 = {id: '1', taskName: '', min: 'A', pomos: 0, done: false};
+    task1 = { id: '1', taskName: '', min: 'A', pomos: 0, done: false };
     tasks.push(task1);
     expect(buttons.calculateTotalTime(tasks)).toBe(false);
 
     // less than 1min
-    task1 = {id: '1', taskName: '', min: '-2', pomos: 0, done: false};
+    task1 = { id: '1', taskName: '', min: '-2', pomos: 0, done: false };
     tasks.push(task1);
     expect(buttons.calculateTotalTime(tasks)).toBe(false);
 
     // more than 180min
-    task1 = {id: '1', taskName: '', min: '200', pomos: 0, done: false};
+    task1 = { id: '1', taskName: '', min: '200', pomos: 0, done: false };
     tasks.push(task1);
     expect(buttons.calculateTotalTime(tasks)).toBe(false);
   });
 
-  test( 'redirect to work timer page', () => {
-        document.body.innerHTML = `
+  test('redirect to work timer page', () => {
+    document.body.innerHTML = `
 			<ul class="tasksList" id="tasksList"></ul>
             <button id="startButton">Start</button>
-        `
+        `;
     let tasks = [];
-    let task1 = {id: '1', taskName: '', min: '5', pomos: 1, done: false};
+    let task1 = { id: '1', taskName: '', min: '5', pomos: 1, done: false };
     tasks.push(task1);
     expect(buttons.calculateTotalTime(tasks)).toBe(true);
     buttons.startSession(tasks);
-		expect( window.location.href).toBe('./../pages/timer.html');		
-	});
-
+    expect(window.location.href).toBe('./../pages/timer.html');
+  });
 }); //end of task page tests
 
 /*describe('onClickQuestionButton', () => {
