c24ea436b734684b64bd6471753b117335c28519
ryanbuui <57205440+ryanbuui@users.noreply.github.com>
2021-03-14 17:34:12 -0700

    Ran formatter and fixed linting errors

--- src/scripts/instructions/instructions.js
+++ src/scripts/instructions/instructions.js
@@ -3,16 +3,15 @@ let currStep = 1;
 window.onload = () => {
   var continueButton = document.getElementById('continueButton');
   continueButton.addEventListener('click', redirectToTasksPage);
-  var stepImage = document.getElementById("stepImage");
+  var stepImage = document.getElementById('stepImage');
   stepImage.src = `../images/step${currStep}.svg`;
-  var leftButton = document.getElementById("stepLeft");
-  var rightButton = document.getElementById("stepRight");
+  var leftButton = document.getElementById('stepLeft');
+  var rightButton = document.getElementById('stepRight');
 
   rightButton.addEventListener('click', () => {
     if (currStep < 11) {
       currStep = currStep + 1;
       stepImage.src = `../images/step${currStep}.svg`;
-      console.log(currStep);
     }
   });
 
@@ -23,8 +22,6 @@ window.onload = () => {
     }
   });
 };
-
-
 
 function redirectToTasksPage() {
   window.location.href = '../pages/tasks.html';
